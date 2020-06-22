64256569c87018c80c19f995a0309230c5b3cedd
Bianca Souza <53984988+SouBia@users.noreply.github.com>
2020-06-22 19:09:07 -0300

    Update database.js
    Trocando a rota do banco de dados

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb://localhost/crud-mongodb';
+const uri = 'mongodb+srv://process.env.USER_MONGO:process.env.PASSWORD_MONGO@cluster0-w4tnw.mongodb.net/crud-mongodb?retryWrites=true&w=majority';
 
 mongoose.connect(uri,
     {
