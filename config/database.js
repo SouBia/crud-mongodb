a2b223e1ee19fe2d47fe73b1ff033d54f145b5bb
Bianca Souza <53984988+SouBia@users.noreply.github.com>
2020-06-22 19:58:04 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb+srv://process.env.USER_MONGO:process.env.PASSWORD_MONGO@cluster0-w4tnw.mongodb.net/crud-mongodb?retryWrites=true&w=majority';
+const uri = "mongodb+srv://dbBianca:process.env.PASSWORD_MONGO@cluster0-w4tnw.mongodb.net/crud-mongodb?retryWrites=true&w=majority";
 
-mongoose.connect(uri,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }
-);
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
